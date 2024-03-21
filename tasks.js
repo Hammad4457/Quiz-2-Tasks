dcce5beac9ef6da5299725544df8d842483412fc
Muhammad Hammad <hammadkhalil04@.com>
2024-03-22 00:45:41 +0500

    Update:"Enhanced"

--- tasks.js
+++ tasks.js
@@ -2,15 +2,14 @@
 console.log("Flattening Array Problem 1");
 console.log(" ");
 
-const array = [1, [[2, 3], 4], [5, 6], 7];
-console.log(array);
-console.log(array.flat(2));
-
 //flatArray = (array) => array.reduce((h1, h2) => h1.concat(h2));
 
 function flatArray(arr) {
   return arr.reduce((h1, h2) => h1.concat(h2), []); //ask sir about returning array []
 }
+
+const array = [1, [[2, 3], 4], [5, 6], 7];
+console.log(array);
 flatArray(array);
 
 // flattening Array Problem 2
